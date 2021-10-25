07d61bb42ab0d09b8eaa19f9873ba4dc0e1739b4
lucas.marcondes <lucas.marcondes@jet.com.br>
2021-10-25 19:51:12 -0300

    Projeto Bytebank

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:bytebank_contacts/screen/dashboard.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(BytbankApp());
+}
+
+class BytbankApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.green[900],
+        accentColor: Colors.blueAccent[700],
+        buttonTheme: ButtonThemeData(
+          buttonColor: Colors.blueAccent[700],
+          textTheme: ButtonTextTheme.primary,
+        ),
+      ),
+      home: Dashboard()
+    );
+  }
+}
+
